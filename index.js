a59c36fe94b9b316b73038b35a739506d2fcf5b6
shiyingjie <shiyingjie@rr.tv>
2022-10-28 16:46:02 +0800

    fix

--- index.js
+++ index.js
@@ -1,71 +1,132 @@
 const Koa = require('koa');
 const app = new Koa();
 const axios = require('axios')
-const token = 'b36bb154f59a79301e0e1f0ed243d640'
+const open = require('open')
+const FormData = require("form-data")
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIxMzY0MTgzNjY5MCIsInNvdXJjZSI6InBjIiwidHlwZSI6ImN1c3RvbWVyIiwiZXhwIjoxNjY3NTI4NjY3LCJzaWduSWQiOiJiZmQ3ZjE0ZTc3MGY0YmFiYTY3M2M2YzVkYmE5NTU5NyIsImlhdCI6MTY2NjkyMzg2N30.WuXhR-h0Mmh5kdDhc4FAWYky1_SVE_3MvkFk8mRJbNA'
 const instance = axios.create({
-  baseURL: 'https://app.chaoyishu.art/api.php/',
+  baseURL: 'https://api.gandart.com/market/api/v2/',
   timeout: 2000,
   headers: {
-    origin: 'https://app.chaoyishu.art',
-    'content-type': 'application/json;charset=UTF-8',
+    origin: 'https://api.gandart.com',
+    Host: 'api.gandart.com',
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
     'token': token
   }
 });
-let price = 600
+const instance2 = axios.create({
+  baseURL: 'https://api.gandart.com/base/v2/',
+  timeout: 2000,
+  headers: {
+    origin: 'https://api.gandart.com',
+    Host: 'api.gandart.com',
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+    'token': token
+  }
+});
+let price = 470
 let key = {
-  261: '九尾狐',
-  262:'火凤',
-  263:'云麒麟'
+  111: '拾荒者',
+  84: '探索者I-Shift'
 }
 let getList = () => {
-  instance.post("document/packmarket", {
-    "page": 1,
-    "name": "",
-    "price": "asc",
-    "p_type": "",
-    "is_sale": 1,
-    "category_id": "261"
-  }).then((res) => {
+  let params = new FormData()
+  params.append('castingId', 84)
+  params.append('page', 1)
+  params.append('pageSize', 15)
+  params.append('sort', 2)
+  params.append('transactionStatus', 2)
+  instance.post("resaleManage/resale/onSale", params).then((res) => {
     let f = []
-    res.data.result.filter((item) => {
-      if (item.price <= price) {
-        f.push(item.id)
+    res.data.obj.list.forEach(item => {
+      if (parseFloat(item.resalePrice) <= price && item.transactionStatus == 2) {
+        f.push(item)
       }
     })
     console.log(f)
     if (f.length) {
-
-      createsellbuy(f)
-    } else {
-      getList()
+      collectionDetails(f.slice(0, 1))
     }
+    else {
+      setTimeout(() => {
+        getList()
+
+      }, 1000)
+    }
+  }).catch(() => {
+    console.log('error:list')
+    setTimeout(() => {
+      getList()
+
+    }, 1000)
   })
 }
 
-let createsellbuy = async (list) => {
-  for (let id of list){
-    instance.post("order/createsellbuy", {
-      id: id,
-      "token": token
-    }).then((res) => {
-      console.log('res1',res)
-      created()
-      return
-    });
-    
+let collectionDetails = (f) => {
+
+
+  let arr = []
+  for (let item of f) {
+    let params = new FormData()
+    params.append('transactionRecordId', item.id)
+    instance.post("resaleManage/resale/collectionDetails", params).then((res) => {
+
+      buy(item.id, res.data.obj)
+
+    }).catch(e => {
+      console.log('error')
+
+      setTimeout(() => {
+        getList()
+
+      }, 1000)
+    })
   }
- 
+
 }
 
-let created = () => {
-    instance.post("user/userorder", {
-      "status": 0,
-      "page": 1,
-      "token": token
-    }).then((res) => {
-      console.log('res2',res)
-    });
+let buy = (transactionRecordId, item) => {
+  console.log(item)
+  let params = new FormData()
+  params.append('castingId', item.castingId)
+  params.append('detailId', item.detailId)
+  params.append('transactionRecordId', transactionRecordId)
+  params.append('userId', item.userId)
+  instance2.post("resaleManage/resale/buy", params).then((res) => {
+    console.log('res1', res.data)
+
+    if (res.data.success) {
+      console.log(res.data)
+      open('www.baidu.com?a=asdasdasdasdasdasddddddddddddd', 'chrome')
+    }
+    else {
+      setTimeout(() => {
+        getList()
+
+      }, 1000)
+    }
+
+  }).catch(e => {
+    setTimeout(() => {
+      getList()
+
+    }, 1000)
+  })
+
+
 }
+
+// let created = () => {
+//   instance.post("user/userorder", {
+//     "status": 0,
+//     "page": 1,
+//     "token": token
+//   }).then((res) => {
+//     console.log('res2', res)
+//   });
+// }
 
 getList()
 
