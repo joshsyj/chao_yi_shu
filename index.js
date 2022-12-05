b4eae88358a637938c5744c67f4832d447167aaf
shiyingjie <shiyingjie@rr.tv>
2022-12-05 10:52:41 +0800

    test

--- index.js
+++ index.js
@@ -3,22 +3,21 @@ const app = new Koa();
 const axios = require('axios')
 const open = require('open')
 const FormData = require("form-data")
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIxMzY0MTgzNjY5MCIsInNvdXJjZSI6InBjIiwidHlwZSI6ImN1c3RvbWVyIiwiZXhwIjoxNjY3Nzg2NTIwLCJzaWduSWQiOiJlZWJkYjU4ZDhhMWQ0NjNlYTRiOTU1ZTA2MjcwY2JlZiIsImlhdCI6MTY2NzE4MTcyMH0.fNt7qxREeOY3D9uvEOOuMyQbTuueehlkcU2Hwcu_mfI'
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIxNTMwMDQ4NTcwOSIsInNvdXJjZSI6InBjIiwidHlwZSI6ImN1c3RvbWVyIiwiZXhwIjoxNjcwMzI5NDc5LCJzaWduSWQiOiJjMDVmN2FiYjU3Mjc0NWI4YmU3MDdiOGQwNjBmYmY1NiIsImlhdCI6MTY2OTcyNDY3OX0.9EVj3OvOVgWcarvcA-sqQEVOgAlD2qhqpJj99nDCajs'
 const instance = axios.create({
   baseURL: 'https://api.gandart.com/market/api/v2/',
-  timeout: 2000,
+  timeout: 4000,
   headers: {
     origin: 'https://api.gandart.com',
     Host: 'api.gandart.com',
     'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
-    'token': token
   }
 });
 
 const instance2 = axios.create({
   baseURL: 'https://api.gandart.com/base/v2/',
-  timeout: 2000,
+  timeout: 4000,
   headers: {
     origin: 'https://api.gandart.com',
     Host: 'api.gandart.com',
@@ -27,7 +26,7 @@ const instance2 = axios.create({
     'token': token
   }
 });
-let price = 2000
+let price = 2500
 let key = {
   111: '拾荒者',
   84: '探索者I-Shift',
@@ -35,12 +34,17 @@ let key = {
   112: '龙蛋',
   95: '提灯',
   101: '凤羽泪',
-  29: '云木'
+  29: '云木',
+  148: '合金',
+  129: 'x型电池',
+  132: '非礼勿听木匣',
+  101: '风',
+  126: '青铜石像'
 }
 let getList = () => {
 
   let params = new FormData()
-  params.append('castingId', 29)
+  params.append('castingId', 31)
   params.append('page', 1)
   params.append('pageSize', 15)
   params.append('sort', 2)
@@ -63,14 +67,14 @@ let getList = () => {
       setTimeout(() => {
         getList()
 
-      }, 1000)
+      }, 400)
     }
   }).catch(() => {
     console.log('error:list')
     setTimeout(() => {
       getList()
 
-    }, 1000)
+    }, 400)
   })
 }
 
@@ -91,7 +95,7 @@ let collectionDetails = (f) => {
       setTimeout(() => {
         getList()
 
-      }, 1000)
+      }, 400)
     })
   }
 
@@ -115,14 +119,14 @@ let buy = (transactionRecordId, item) => {
       setTimeout(() => {
         getList()
 
-      }, 1000)
+      }, 400)
     }
 
   }).catch(e => {
     setTimeout(() => {
       getList()
 
-    }, 1000)
+    }, 400)
   })
 
 
